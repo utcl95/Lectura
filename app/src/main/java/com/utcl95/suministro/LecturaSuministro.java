f7dcbaec607729c3cbdcf0830961c2e8371a1a94
Juan Aquino <utcl95@gmail.com>
2016-04-16 23:49:26 -0500

    First Commit

---
+++ app/src/main/java/com/utcl95/suministro/LecturaSuministro.java
@@ -0,0 +1,13 @@
+package com.utcl95.suministro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LecturaSuministro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lectura_suministro);
+    }
+}
---
+++ app/src/main/java/com/utcl95/suministro/ModificarLectura.java
@@ -0,0 +1,13 @@
+package com.utcl95.suministro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModificarLectura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_lectura);
+    }
+}
